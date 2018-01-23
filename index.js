422628009e9f1f8b430f46c459d3ebc7ed32af8e
yalamber Subba <yalamber@mundhum.com>
2018-01-23 23:05:03 +0545

    Fixes

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+const fs = require('fs');
 const countries = require('./countries.json');
 
-module.exports = countries;
+exports.getCountry = countries;
+
+exports.getProvince = (province, callback) => {
+  fs.readFile(`./province/${province}`, 'utf8', function (err, data) {
+    if (err) {
+      return callback(err);
+    }
+    let data = JSON.parse(data);
+    return callback(null, data);
+  });
+};
+
+exports.getDistrict = (district) => {
+  fs.readFile(`./districts/${district}`, 'utf8', function (err, data) {
+    if (err) {
+      return callback(err);
+    }
+    let data = JSON.parse(data);
+    return callback(null, data);
+  });
+};
